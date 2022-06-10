b4dacb8a0f96773d2b891d5d694f25e6d89133bc
Kc5f <Kc5f>
2022-06-09 19:50:02 -0500

    it works!!!!!!!

---
+++ index.js
@@ -0,0 +1,27 @@
+const WebSocket = require('ws');
+const axios = require('axios');
+const config = require('./config.js');
+
+const ws = new WebSocket(`wss://chat.switchcraft.pw/${config.chatboxtoken}`);
+
+ws.on('message', async (data) => {
+    data = JSON.parse(data.toString());
+    console.log(data);
+    if (data.type == 'message' && data.text.toLowerCase().includes(config.myname)) {
+        let channelType;
+        let username;
+        if (data.discordUser) {
+            channelType = 'Discord';
+            username = `${data.discordUser.name}#${data.discordUser.discriminator}`;
+        } else if (data.user) {
+            channelType = 'In-game';
+            username = data.user.name;
+        };
+        console.log(channelType, username, data.text)
+        const message = encodeURIComponent(`Mention from \`${channelType}\`/\`${username}\`:\n\`${data.text}\``);
+        console.log(message);
+        axios.post(`https://api.telegram.org/bot${config.telegramtoken}/sendMessage?chat_id=${config.telegramid}&text=${message}&parse_mode=MarkdownV2`).catch((err) => {
+            console.log(err);
+        });
+    };
+});
